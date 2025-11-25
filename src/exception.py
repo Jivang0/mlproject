d4f92e2016561549dc53218ca2e2fd3743ac3d85
Jivan Gaire <jivangaire71@gmail.com>
2025-11-25 14:18:53 +0545

    Data Transformation Done

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 
 def error_message_details(error,error_details:sys):
-    _,_,exc_tb = error_details.exe_info()
+    _,_,exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".formate(
              file_name,exc_tb.tb_lineno,str(error))
